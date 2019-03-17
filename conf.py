8f98e5cd8e209e31313621b91cc247a98dd57e52
tdasgupt <tathagata.dasgupta@here.com>
2019-03-17 14:18:28 -0500

    Nikola auto commit.
    Nikola version: 8.0.2

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Chicago Python User Group"  # (translatable)
-BLOG_TITLE = "ChiPy Coding Workshops"  # (translatable)
+BLOG_TITLE = "Python Project Night Challenges"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://chicagopython.github.io/"
